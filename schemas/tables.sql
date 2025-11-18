03f47e37848ddd3d455566efe535d30c71fadde2
Yasith Dharmasena <yasith@calcey.com>
2025-11-18 08:12:07 +0530

    Update tables.sql with country column

--- schemas/tables.sql
+++ schemas/tables.sql
@@ -5,9 +5,10 @@ CREATE TABLE sms_meeting_log (
     encrypted_payload  TEXT NOT NULL,
     decrypted_message  TEXT NOT NULL,
     raw_sms            TEXT NOT NULL,
+    country            VARCHAR(8),
     created_at         TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at         TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    PRIMARY KEY (group_id, meeting_id)
+    PRIMARY KEY (group_number, meeting_number)
 );
 
 CREATE TABLE sms_failed_decrypt_log (
